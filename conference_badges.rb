71db4d668b48c8a03e2ffa8f3b44c6f83f59cc94
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-07 18:12:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges = []
+  for name in array
+    badges << badge_maker(name)
+  end
+  return badges
+end
+
+def assign_rooms(array)
+  room = []
+  room << array.each_with_index do |name, num|
+    "Hello, #{name}! You'll be assigned to room #{num + 1}!"
+  end
+  return room
+end
